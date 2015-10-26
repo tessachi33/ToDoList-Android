a641e4563bfd1c94224f30475171f52f9ba067b4
Tessa <Tessachi33@yahoo.com>
2015-10-26 13:28:27 -0700

    Add java for working app

--- app/src/main/java/io/github/tessachi33/todolist/MainActivity.java
+++ app/src/main/java/io/github/tessachi33/todolist/MainActivity.java
@@ -1,31 +1,45 @@
 package io.github.tessachi33.todolist;
 
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+
+public class MainActivity extends ListActivity {
+
+    private ArrayList<String> mTasks;
+    private Button mTaskButton;
+    private EditText mTaskText;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        mTaskButton = (Button) findViewById(R.id.taskButton);
+        mTaskText = (EditText) findViewById(R.id.taskText);
+        mTasks = new ArrayList<String>();
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mTasks);
+        setListAdapter(mAdapter);
+
+        mTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                addTask();
             }
         });
     }
-}
 
+    private void addTask() {
+        String newTask =  mTaskText.getText().toString();
+        mTasks.add(newTask);
+        mAdapter.notifyDataSetChanged();
+    }
+}
